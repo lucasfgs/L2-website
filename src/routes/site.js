5b9585f8604044b432ab7efa47896fb0cbb63836
Lucas Ferreira <blooodcf@gmail.com>
2020-04-23 01:41:27 -0300

    Downloads page

--- src/routes/site.js
+++ src/routes/site.js
@@ -8,14 +8,18 @@ const routes = Router();
 
 routes.get("/", async (req, res) => {
   getGameserverStatus()
-    .then(async serverStatus => {
+    .then(async (serverStatus) => {
       const playersOnline = await getOnlinePlayers();
 
       res.render("home", { playersOnline, serverStatus });
     })
-    .catch(error => {
+    .catch((error) => {
       res.render("home", { playersOnline: 0, serverStatus: "Offline" });
     });
+});
+
+routes.get("/download", async (req, res) => {
+  res.render("download");
 });
 
 routes.get("/register", async (req, res) => {
@@ -25,21 +29,17 @@ routes.get("/register", async (req, res) => {
 routes.post(
   "/register",
   [
-    check("name")
-      .notEmpty()
-      .withMessage("Name cannot be empty"),
+    check("name").notEmpty().withMessage("Name cannot be empty"),
     check("password")
       .isLength({ min: 6 })
       .withMessage("Password must contain at least 6 characters"),
-    check("email")
-      .isEmail()
-      .withMessage("Invalid e-mail"),
+    check("email").isEmail().withMessage("Invalid e-mail"),
     check("login")
       .isLength({ min: 5 })
       .withMessage("Login must contain at least 5 characters"),
     check("terms")
       .exists()
-      .withMessage("Please confirm our terms & conditions")
+      .withMessage("Please confirm our terms & conditions"),
   ],
   AccountController.createAccount
 );
