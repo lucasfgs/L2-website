f72c2d7ed9d9f665b82fd8a679a6133a29fa7152
Lucas Ferreira <blooodcf@gmail.com>
2020-04-27 22:40:57 -0300

    Favicon

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
 import serveIndex from "serve-index";
+import favicon from "express-favicon";
 import cors from "cors";
 
 import { Site, Api } from "./routes";
@@ -14,12 +15,14 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(cors());
 app.use(express.static("public"));
+app.use(favicon("public/images/favicon.ico"));
 app.use("/updater", serveIndex("public/updater"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(Site);
 app.use("/api", Api);
 
-dbModels.sequelize.sync().then(() => {
-  app.listen(process.env.PORT || 80, () => console.log("Listening"));
-});
+// dbModels.sequelize.sync().then(() => {
+// });
+
+app.listen(process.env.PORT || 80, () => console.log("Listening"));
