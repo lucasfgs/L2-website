7ba79794fd5234e0dcc721d3c75e1f7c09c31992
Lucas Ferreira <blooodcf@gmail.com>
2020-04-27 23:31:21 -0300

    Test deploy

--- src/server.js
+++ src/server.js
@@ -5,11 +5,26 @@ import bodyParser from "body-parser";
 import serveIndex from "serve-index";
 import favicon from "express-favicon";
 import cors from "cors";
+import greenlock from "greenlock-express";
 
 import { Site, Api } from "./routes";
 import dbModels from "./models";
 
 const app = express();
+// console.log(__dirname);
+// greenlock
+//   .init({
+//     packageRoot: "./",
+
+//     // contact for security and critical bug notices
+//     configDir: "./src/greenlock.d",
+//     maintainerEmail: "blooodcf@gmail.com",
+//     // whether or not to run at cloudscale
+//     cluster: false,
+//   })
+//   // Serves on 80 and 443
+//   // Get's SSL certificates magically!
+//   .serve(app);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -23,6 +38,7 @@ app.use(Site);
 app.use("/api", Api);
 
 // dbModels.sequelize.sync().then(() => {
+
 // });
 
 app.listen(process.env.PORT || 80, () => console.log("Listening"));
