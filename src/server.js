a5a91bbbf5b84f680ceefe9d4b71f9ff1ba5bdba
Lucas Ferreira <blooodcf@gmail.com>
2020-04-28 02:22:05 -0300

    SSL certficate

--- src/server.js
+++ src/server.js
@@ -5,26 +5,13 @@ import bodyParser from "body-parser";
 import serveIndex from "serve-index";
 import favicon from "express-favicon";
 import cors from "cors";
-import greenlock from "greenlock-express";
+import https from "https";
+import fs from "fs";
 
 import { Site, Api } from "./routes";
 import dbModels from "./models";
 
 const app = express();
-// console.log(__dirname);
-// greenlock
-//   .init({
-//     packageRoot: "./",
-
-//     // contact for security and critical bug notices
-//     configDir: "./src/greenlock.d",
-//     maintainerEmail: "blooodcf@gmail.com",
-//     // whether or not to run at cloudscale
-//     cluster: false,
-//   })
-//   // Serves on 80 and 443
-//   // Get's SSL certificates magically!
-//   .serve(app);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -37,8 +24,14 @@ app.use(bodyParser.json());
 app.use(Site);
 app.use("/api", Api);
 
-// dbModels.sequelize.sync().then(() => {
-
-// });
-
-app.listen(process.env.PORT || 80, () => console.log("Listening"));
+dbModels.sequelize.sync().then(() => {
+  https
+    .createServer(
+      {
+        key: fs.readFileSync("key.pem"),
+        cert: fs.readFileSync("./cert.pem"),
+      },
+      app
+    )
+    .listen(process.env.PORT || 80, () => console.log("Listening"));
+});
