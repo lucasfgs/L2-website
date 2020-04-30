1c6bfbedca8386a18378279b3625232cf4a3eec2
n3mo0 <andre.marra@live.com>
2020-04-30 07:32:19 -0300

    Home, Download,mediabox, link, logopng

--- src/routes/site.js
+++ src/routes/site.js
@@ -6,49 +6,49 @@ import { getGameserverStatus } from "../services/gameserver";
 
 const routes = Router();
 
-routes.get("/", (req, res) => {
-  res.render("countdown.ejs");
-});
+// routes.get("/", (req, res) => {
+//   res.render("countdown.ejs");
+// });
 
 // ATIVAR QUANDO O SITE ENTRAR AO AR
-// routes.get("/", async (req, res) => {
-//   getGameserverStatus()
-//     .then(async (serverStatus) => {
-//       const playersOnline = await getOnlinePlayers();
+routes.get("/", async (req, res) => {
+  getGameserverStatus()
+    .then(async (serverStatus) => {
+      const playersOnline = await getOnlinePlayers();
 
-//       res.render("home", { playersOnline, serverStatus });
-//     })
-//     .catch((error) => {
-//       res.render("home", { playersOnline: 0, serverStatus: "Offline" });
-//     });
-// });
+      res.render("home", { playersOnline, serverStatus });
+    })
+    .catch((error) => {
+      res.render("home", { playersOnline: 0, serverStatus: "Offline" });
+    });
+});
 
-// routes.get("/download", async (req, res) => {
-//   res.render("download");
-// });
+routes.get("/download", async (req, res) => {
+  res.render("download");
+});
 
-// routes.get("/register", async (req, res) => {
-//   res.render("register");
-// });
+routes.get("/register", async (req, res) => {
+  res.render("register");
+});
 
-// routes.post(
-//   "/register",
-//   [
-//     check("name").notEmpty().withMessage("Name cannot be empty"),
-//     check("password")
-//       .isLength({ min: 6 })
-//       .withMessage("Password must contain at least 6 characters"),
-//     check("email").isEmail().withMessage("Invalid e-mail"),
-//     check("login")
-//       .isLength({ min: 5 })
-//       .withMessage("Login must contain at least 5 characters"),
-//     check("terms")
-//       .exists()
-//       .withMessage("Please confirm our terms & conditions"),
-//   ],
-//   AccountController.createAccount
-// );
+routes.post(
+  "/register",
+  [
+    check("name").notEmpty().withMessage("Name cannot be empty"),
+    check("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must contain at least 6 characters"),
+    check("email").isEmail().withMessage("Invalid e-mail"),
+    check("login")
+      .isLength({ min: 5 })
+      .withMessage("Login must contain at least 5 characters"),
+    check("terms")
+      .exists()
+      .withMessage("Please confirm our terms & conditions"),
+  ],
+  AccountController.createAccount
+);
 
-// routes.get("/confirm", AccountController.confirmAccount);
+routes.get("/confirm", AccountController.confirmAccount);
 
 export default routes;
